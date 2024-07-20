0f9d99ebe03eb80b978fe9113ddaadffabc8380b
w1thluv <dimahudric@gmail.com>
2024-07-20 08:18:39 +0300

    Fix bug.

--- AskaHelper.Daemon/Services/OsInteraction/OsModules/WindowsDependent/WindowsDriveService.cs
+++ AskaHelper.Daemon/Services/OsInteraction/OsModules/WindowsDependent/WindowsDriveService.cs
@@ -5,6 +5,6 @@ namespace AskaHelper.Daemon.Services.OsInteraction.OsModules.WindowsDependent;
 internal static class WindowsDriveService {
     public static DriveInfo[] Analyze() {
         Debug.Assert(Aska.OsIdentity.Family == OsFamily.Windows);
-        return DriveInfo.GetDrives();
+        return DriveInfo.GetDrives().Where(drive => drive.IsReady == true).ToArray();
     }
 }
